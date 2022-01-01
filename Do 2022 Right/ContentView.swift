949c76a824bfca479c8031954144d74bd1390575
LuckDuracell <lukedru@yahoo.com>
2022-01-01 01:23:12 -0800

    Initial Commit

---
+++ Do 2022 Right/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Do 2022 Right
+//
+//  Created by Luke Drushell on 1/1/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Do 2022 Right/Do_2022_RightApp.swift
@@ -0,0 +1,17 @@
+//
+//  Do_2022_RightApp.swift
+//  Do 2022 Right
+//
+//  Created by Luke Drushell on 1/1/22.
+//
+
+import SwiftUI
+
+@main
+struct Do_2022_RightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
